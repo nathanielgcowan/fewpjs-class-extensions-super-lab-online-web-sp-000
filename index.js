9b69ec54670b34b6088a841d826c3142b5280854
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-21 17:48:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your classes here
+
+//- A `Tree` instance should take in one parameter when created, `species`,
+  // and assign this to a property named `species`.
+  class Tree {
+    constructor(species) {
+      this.species = species;
+    }
+    // - A `Tree` should have a static method, `definition()`, that returns a short
+  // definition of all trees.
+    static definition() {
+      return `a short definition of all trees.`;
+    }
+  }
