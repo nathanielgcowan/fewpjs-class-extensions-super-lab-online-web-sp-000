ae9f5c0bc1dbce1d58bf1db20592ee21a8381347
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-21 17:57:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,3 +29,13 @@
       return super.definition()+` Deciduous trees shed their leaves annually.`
     }
   }
+
+  class Evergreen extends Tree {
+    constructor(species,name) {
+      super(species);
+      this.name = name;
+    }
+    static definition() {
+      return super.definition()+ ` Evergreens keep their leaves all year round.`
+    }
+  }
