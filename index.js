036cedf9624f25f97676bfaa2d3006f35e7cfc15
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-21 17:53:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,20 @@
       return `A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.`;
     }
   }
+
+  // - A `Deciduous` instance takes two parameters, `species` and `name`. Use
+  //   `super()` in the constructor to use the parent class constructor to assign
+  //   `species`. After `super()`, assign the `name` parameter to the a `name`
+  //   property in the `Deciduous` constructor
+  class Deciduous extends Tree {
+    constructor(species, name) {
+      super(species);
+      this.name = name;
+    }
+  //   - Create a static method, `definition()`, that uses `super` to access
+  // `definition()` from `Tree` and add the following to the provide a specific
+  // definition for `Deciduous`:
+    static definition() {
+      return `super.name trees shed their leaves annually.`
+    }
+  }
